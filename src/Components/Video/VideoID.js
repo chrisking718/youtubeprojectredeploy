729e2412c0aa7f2c30d2b5c7ef99e5613782d732
chrisking718 <chrisking@pursuit.org>
2022-11-21 12:24:37 -0500

    working

--- src/Components/Video/VideoID.js
+++ src/Components/Video/VideoID.js
@@ -1,62 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
-
-
-
-import YouTube from "react-youtube";
-
 import Comment from "./Comment"
 import CommentData from "./CommentData"
 import Form from "./Form"
-
+import YouTube from "react-youtube";
 import "./VideoID.css"
 
- 
-
-export default function VideoID(){   
-
+export default function VideoID({colors}){   
 const [comment, setComment] = useState(CommentData)
 const {id} = useParams()
 
-// const ref = firebase.firestore().collection("youtube_comments/:id")
-
-
-
-const [storedResults, setStoredResults] = useState([]);
-
-// useEffect(() => {
-//    const items = JSON.parse(localStorage.getItem('stored-results'));
-//   if (storedResults) {
-//     setStoredResults(items);
-//   }
-  
-// }, []);
-
 function handleComment(comments){
-    
-    // ref.add({
-        //     name: "comment.name",
-        //     comment: "comment.data"
-        // })
-        
-        setComment([...comment, comments])
-        
-        
-    }
-    console.log("stored results",storedResults)
-
-
-
+    setComment([...comment, comments])
+}
 
 return(
-
     <section>
+        
         <YouTube
         videoId={id}
         className={"youtube-video"}
         />
-        <Form handleComment={handleComment}/>
-        <Comment comment={comment}/>
+        <br></br>
+        <section className="comment-section">
+        <Form colors={colors} handleComment={handleComment}/>
+        <Comment colors={colors} setComment={setComment}comment={comment}/>
+        </section>
     </section>
 )
 }
