2c5a5eab9d179a633bf9e16f5bd0938a059256ab
chrisking718 <chrisking@pursuit.org>
2022-11-21 12:25:19 -0500

    more work

--- src/Components/Nav/home.js
+++ src/Components/Nav/home.js
@@ -1,62 +1,66 @@
-import { useState, useEffect} from "react"
-
+import { useState, useRef} from "react"
+import ReactPaginate from "react-paginate";
 import ResultError from "../errors/ResultsError"
 import Video from "../Video/Video"
 import {getYoutubeVideos} from '../../api/fetch'
 
 import "./home.css"
 
+const pageData = 3
+
 export default function Home () {
 const [youtube , setYoutube] = useState([])
-const [resultError, setResultError] = useState(false)
+const [currentPage, setCurrentPage] = useState(0)
+ const [resultError, setResultError] = useState(false)
 const [search, setSearch] = useState("");
-const [storedResults, setStoredResults] = useState([])
 
-
+const ref = useRef(null)
 
 // this function calls the fetch with the search results, sets the results.items into the youtube array.
-   function getResults(){
-    if (search.length > 0){
-    getYoutubeVideos(search)
-     .then((response) => {
-         setYoutube(response.items);
-         setResultError(true)
-       })
-       .catch((error) => {
 
-       });
-
-    }
-   }
+// this function gets the value that the user typed in the search bar and sets it to the search state  
+function handleTextChange(event) {
+  let title = event.target.value; 
+  setSearch(title)
+}
 
 
-    // useEffect(() => {
-    // localStorage.setItem('stored-results', JSON.stringify(youtube));
-    // }, [search]);
 
+function getResults(){
+ if (search.length > 0){
+ getYoutubeVideos(search)
+  .then((response) => {
+    setSearch("")
+      setYoutube(response.items);
+      setResultError(true)
+      ref.current.value = ''
+    })
+    .catch((error) => {
 
-    // useEffect(() => {
-    //     const items = JSON.parse(localStorage.getItem('stored-results'));
-    //     if (storedResults) {
-    //      setStoredResults(items);
-    //     }
-    //   }, []);
+    });
 
-  // this function gets the value that the user typed in the search bar and sets it to the search state  
-   function handleTextChange(event) {
-    const title = event.target.value;
-  
-  
-    setSearch(title);
-
-  
-  }
+ }
+}
 //   console.log(youtube)
-//   console.log(search.length)
-console.log(localStorage.getItem('stored-results'))
+
+function handlePageChange ({selected: selectedPage}){
+  setCurrentPage(selectedPage)
+}
+const offSet = currentPage * pageData
+
+const currentPageData = youtube
+.slice(offSet, offSet + pageData)
+.map((yt) => <Video key={yt.id.videoId} yt={yt}/>)
+
+const pageCount = Math.ceil(youtube.length/pageData) 
+
+
+
     return (
         <div className="search-feature">
            <input
+           ref={ref}
+           autoComplete = "off"
             className="search"
             type="text"
             placeholder="Search..."
@@ -66,17 +70,24 @@ console.log(localStorage.getItem('stored-results'))
         <div>
             <br></br>
             <br></br>
+            {currentPageData.length === 0 ? null :
 
-            {!resultError ? (<ResultError/>):(
+            <ReactPaginate
+         previousLabel={"Previous"}
+         nextLabel={"Next"}
+         pageCount={pageCount}
+         onPageChange={handlePageChange}
+         containerClassName={"pagination"}
+         previousLinkClassName={"pagination-link"}
+         nextLinkClassName={"pagination-link"}
+         />  
+            }
+        
+         {!resultError ? (<ResultError/>):(
             <div className="thumbnails">
-                
        
         {
-           storedResults.map((yt) => {
-               return (
-                   <Video key={yt.id.videoId} yt={yt}/>
-               )
-           })
+           currentPageData
         }
             </div>
             )}
